7c682f1be587281cd91457a87ad41073106c3b33
x10forevers <android.x10f@gmail.com>
2018-11-27 04:04:55 +0000

    bring up lineage

--- lineage_g8341.mk
+++ lineage_g8341.mk
@@ -12,15 +12,40 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
 TARGET_KERNEL_CONFIG := aosp_yoshino_poplar_defconfig
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/poplar/device.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/lineage_base_telephony.mk)
 
-PRODUCT_NAME := lineage_g8341
+### BOOTANIMATION
+# vendor/lineage/config/common_full_phone.mk
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+# vendor/lineage/config/common.mk
+TARGET_BOOTANIMATION_HALF_RES := true
+
+### LINEAGE
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := lineage_poplar
 PRODUCT_DEVICE := poplar
-PRODUCT_MODEL := Xperia XZ1
 PRODUCT_BRAND := Sony
+PRODUCT_MODEL := G8341
 PRODUCT_MANUFACTURER := Sony
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=poplar \
+BUILD_FINGERPRINT=Sony/poplar/poplar:9.0.0/PQ1A.181105.017.A1/1:user/dev-keys
+
+ifneq ($(LINEAGE_DEV_CERTIFICATE),)
+    PRODUCT_DEFAULT_DEV_CERTIFICATE := $(LINEAGE_DEV_CERTIFICATE)
+endif
+
+ifneq ($(LINEAGE_VERITY_CERTIFICATE),)
+    PRODUCT_VERITY_SIGNING_KEY := $(LINEAGE_VERITY_CERTIFICATE)
+endif
