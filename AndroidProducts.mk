daa23f32bbe790bd6e106c43d8dab461c28da105
x10forevers <android.x10f@gmail.com>
2018-11-27 03:40:51 +0000

    Bring up Lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/aosp_g8341.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/lineage_g8341.mk
 
