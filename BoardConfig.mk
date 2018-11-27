ca652d737b159fb227f4ff272d5a8eda2a7771d9
x10forevers <android.x10f@gmail.com>
2018-11-27 03:44:49 +0000

    Bring up Lineage

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,17 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include device/sony/yoshino/PlatformConfig.mk
+### INHERIT FROM YOSHIRO
+include device/sony/yoshino/BoardConfigPlatform.mk
+include vendor/sony/poplar/BoardConfigVendor.mk
 
-TARGET_BOOTLOADER_BOARD_NAME := unknown
-ifneq (,$(filter %g8341,$(TARGET_PRODUCT)))
+#BOOTLOADER
 TARGET_BOOTLOADER_BOARD_NAME := G8341
-else ifneq (,$(filter %g8342,$(TARGET_PRODUCT)))
-TARGET_BOOTLOADER_BOARD_NAME := G8342
-else
-TARGET_BOOTLOADER_BOARD_NAME := G8341
-$(warning Unrecognized value for TARGET_PRODUCT: "$(TARGET_PRODUCT)", using default value: "$(TARGET_BOOTLOADER_BOARD_NAME)")
-endif
 
 # Platform
 PRODUCT_PLATFORM := yoshino
