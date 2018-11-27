b1b70feb4fa45fd2b2d92731b1a9e207fb497012
x10forevers <android.x10f@gmail.com>
2018-11-27 03:55:30 +0000

    Lineage bring-up

--- device.mk
+++ device.mk
@@ -1,116 +1,30 @@
-# Copyright 2014 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+### PLATFORM
+$(call inherit-product, device/sony/yoshino/platform.mk)
+### PROPRIETARY VENDOR FILES
+$(call inherit-product, vendor/sony/poplar/poplar-vendor.mk)
 
-# Device path
-DEVICE_PATH := device/sony/poplar/rootdir
+ifeq ($(WITH_FDROID),true)
+$(call inherit-product, vendor/fdroid/fdroid-vendor.mk)
+endif
+ifeq ($(WITH_MICROG),true)
+$(call inherit-product, vendor/microg/microg-vendor.mk)
+endif
 
-DEVICE_PACKAGE_OVERLAYS += \
-    device/sony/poplar/overlay
+### DALVIK
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
-# Device Specific Permissions
-PRODUCT_COPY_FILES := \
-    frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml
+DEVICE_PATH := device/sony/poplar
 
-# Sensors Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
-
-# Thermal Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/thermanager.xml:$(TARGET_COPY_OUT_VENDOR)/etc/thermanager.xml
-
-# Camera Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/camera/camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_config.xml \
-    $(DEVICE_PATH)/vendor/etc/camera/imx258_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx258_chromatix.xml \
-    $(DEVICE_PATH)/vendor/etc/camera/imx400_chromatix.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/imx400_chromatix.xml
-
-# Audio Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(DEVICE_PATH)/vendor/etc/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml
-
-# Audio calibration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Bluetooth_cal.acdb \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/General_cal.acdb \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Global_cal.acdb \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Handset_cal.acdb \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Hdmi_cal.acdb \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Headset_cal.acdb \
-    $(DEVICE_PATH)/vendor/etc/acdbdata/Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/Speaker_cal.acdb
-
-# Focus calibration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/tof_focus_calibration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/tof_focus_calibration.xml
-
-# NFC Configuration
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
-    $(DEVICE_PATH)/vendor/etc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
-
-# BT/Wifi patch file
-PRODUCT_COPY_FILES += \
-    $(DEVICE_PATH)/vendor/firmware/wlan/qca_cld/bdwlan.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/bdwlan.bin
-
-# Device Init
-PRODUCT_PACKAGES += \
-    fstab.poplar \
-    init.recovery.poplar \
-    init.poplar
-
-# Simple PowerHAL
-PRODUCT_PACKAGES += \
-    power.poplar
-
-# Telephony Packages (AOSP)
-PRODUCT_PACKAGES += \
-    InCallUI \
-    Stk
-
-# Treble
-# Include vndk/vndk-sp/ll-ndk modules
-PRODUCT_PACKAGES += \
-    vndk_package
-
-# SAR
-PRODUCT_PACKAGES += \
-    TransPowerSensors
-
-# Camera Augmented Sensing Helper
-PRODUCT_PACKAGES += \
-   libpolyreg \
-   cashsvr \
-   libcashctl
-
-PRODUCT_AAPT_CONFIG := normal
+# Device uses high-density artwork where available
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-PRODUCT_PROPERTY_OVERRIDES := \
-    ro.sf.lcd_density=480 \
-    ro.usb.pid_suffix=1F3
+DEVICE_PACKAGE_OVERLAYS += \
+    $(DEVICE_PATH)/overlay
 
-# setup dm-verity configs.
-PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/platform/soc/1da4000.ufshc/by-name/vendor
-
-# Inherit from those products. Most specific first.
-$(call inherit-product, device/sony/yoshino/platform.mk)
-
-# copy NFC firmware
-$(call inherit-product-if-exists, vendor/nxp/nxp-vendor.mk)
-
-# include board vendor blobs
-$(call inherit-product-if-exists, vendor/sony/yoshino-common/yoshino-partial.mk)
+ifeq ($(WITH_TWRP),true)
+include $(DEVICE_PATH)/device/init.mk
+else # WITH_TWRP
+include $(DEVICE_PATH)/device/*.mk
+include $(DEVICE_PATH)/vendor_prop.mk
+endif # WITH_TWRP
