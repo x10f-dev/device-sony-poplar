2503c56e8d675cafeeac8761b016df807435d0ba
x10forevers <android.x10f@gmail.com>
2018-11-27 03:31:35 +0000

    Lineage bring up

--- aosp_g8341.mk
+++ aosp_g8341.mk
@@ -17,10 +17,10 @@ TARGET_KERNEL_CONFIG := aosp_yoshino_poplar_defconfig
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/poplar/device.mk)
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/lineage_base_telephony.mk)
 
-PRODUCT_NAME := aosp_g8341
+PRODUCT_NAME := lineage_g8341
 PRODUCT_DEVICE := poplar
-PRODUCT_MODEL := Xperia XZ1 (AOSP)
+PRODUCT_MODEL := Xperia XZ1
 PRODUCT_BRAND := Sony
 PRODUCT_MANUFACTURER := Sony
